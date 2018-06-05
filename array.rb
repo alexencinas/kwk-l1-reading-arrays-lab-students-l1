44598d8d826bb11fb3812328a4fda5ddb95e5b15
alexencinas <shoogiee123@gmail.com>
2018-06-05 21:40:10 +0000

    automatically backed up by learn

--- array.rb
+++ array.rb
@@ -6,3 +6,7 @@ puts students_names
 
 #puts [name of your array] prints out the information in your array in the terminal 
 puts students_names[0]
+
+#position 0 gives you the first value in the array 
+students_names.push('Cardi B')
+puts students_names
